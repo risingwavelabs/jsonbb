54ed4c8287808d7dd37f85d0eb24db5433eeb23a
Runji Wang <wangrunji0408@163.com>
2023-10-12 18:44:26 +0800

    add fuzz project
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

---
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -0,0 +1,10 @@
+#![no_main]
+
+use flat_json::Value;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = s.parse::<Value>();
+    }
+});
